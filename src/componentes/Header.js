fe56d4539ffe5d437336978384aa9c1f65c85092
edgardo <nergumero2017@gmail.com>
2020-05-27 16:41:32 -0300

    first commit

---
+++ src/componentes/Header.js
@@ -0,0 +1,20 @@
+import React, {Fragment} from 'react';
+
+
+/*function Header(titulo) {
+
+    
+    return(
+        <Fragment>
+        <h1>{ titulo}</h1>
+        
+        </Fragment>
+    )
+}
+*/
+const Header = ({titulo}) => (
+    <Fragment>
+        <h1>{ titulo}</h1>
+    </Fragment>
+)
+export default Header;
